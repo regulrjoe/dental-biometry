f83d9071989894d225af264aaf76ca36d595bf08
regulrjoe <joevzq@gmail.com>
2018-03-01 11:51:02 -0600

    Write Open Image function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "controller.h"
+#include <QFileDialog>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,6 +14,29 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionOpen_Image_triggered()
+{
+    QString filename;
+
+    filename = QFileDialog::getOpenFileName(this,
+                                            tr("Open an image file"),
+                                            "~/",
+                                            tr("Image Files (*.png *.jpg *.jpeg *.bmp"));
+
+    if (filename != NULL) {
+        if (Controller::getInstance()->setInputImage(filename.toUtf8().data())) {
+            ui->imgViewer->showImage(Controller::getInstance()->getFilteredImage());
+            ui->btnApplyMedian->setEnabled(true);
+            ui->btnApplyBilateral->setEnabled(true);
+            ui->btnClearFilters->setEnabled(true);
+            ui->btnApplySegmentation->setEnabled(true);
+        } else {
+            std::cout << "Image loading failed." << std::endl;
+            QMessageBox::information(this, tr("Unable to open image"), tr("Select a valid image file."));
+        }
+    }
 }
 
 void MainWindow::on_sliderMedian_sliderMoved(int position)
@@ -48,22 +74,17 @@ void MainWindow::on_btnClearFilters_clicked()
 
 }
 
-void MainWindow::on_spinBox_3_valueChanged(int arg1)
+void MainWindow::on_btnApplySegmentation_clicked()
 {
 
 }
 
-void MainWindow::on_spinBox_4_valueChanged(int arg1)
+void MainWindow::on_numSegmentationLineProfileColumnSpacing_valueChanged(int arg1)
 {
 
 }
 
-void MainWindow::on_pushButton_clicked()
-{
-
-}
-
-void MainWindow::on_actionOpen_Image_triggered()
+void MainWindow::on_numSegmentationLineProfileDerivativeDistance_valueChanged(int arg1)
 {
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,6 +16,8 @@ public:
     ~MainWindow();
 
 private slots:
+    void on_actionOpen_Image_triggered();
+
     void on_sliderMedian_sliderMoved(int position);
 
     void on_sliderBilateral_sliderMoved(int position);
@@ -30,13 +32,11 @@ private slots:
 
     void on_btnClearFilters_clicked();
 
-    void on_spinBox_3_valueChanged(int arg1);
+    void on_btnApplySegmentation_clicked();
 
-    void on_spinBox_4_valueChanged(int arg1);
+    void on_numSegmentationLineProfileColumnSpacing_valueChanged(int arg1);
 
-    void on_pushButton_clicked();
-
-    void on_actionOpen_Image_triggered();
+    void on_numSegmentationLineProfileDerivativeDistance_valueChanged(int arg1);
 
 private:
     Ui::MainWindow *ui;
