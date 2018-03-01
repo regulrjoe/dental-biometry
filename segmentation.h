d938f112417ca84527395aa18f90d306215e9893
regulrjoe <joevzq@gmail.com>
2018-02-28 22:13:09 -0600

    Write setters and getters of _lineprofile_col_spacing and _lineprofile_derivative_distance

--- segmentation.h
+++ segmentation.h
@@ -21,31 +21,61 @@ public:
     // Run algorithm
     cv::Mat Process(const cv::Mat&);
 
-    /* Space for getters and setters */
-    // _lineprofile_col_spacing
-    // _lineprofile_derivative_distance
+    //// SETTERS AND GETTERS ////
+    // Get line profiles column spacing
+    int getLineProfileColSpacing() {
+        return _lineprofile_col_spacing;
+    }
 
+    // Set line profiles column spacing
+    bool setLineProfileColSpacing(const int& cs) {
+        if (cs < 1 || cs > 100)
+            return false;
+        _lineprofile_col_spacing = cs;
+        return true;
+    }
+
+    // Get line profiles derivative distance
+    int getLineProfileDerivativeDistance() {
+        return _lineprofile_derivative_distance;
+    }
+
+    // Set line profiles derivative distance
+    bool setLineProfileDerivativeDistance(const int& dd) {
+        if (dd < 1 || dd > 100)
+            return false;
+        _lineprofile_derivative_distance = dd;
+        return true;
+    }
 
 private:
+    //// ATTRIBUTES ////
     // Local copy of input image
     cv::Mat _image;
+
     // Local copy of _image for drawing and displaying
     cv::Mat _display_image;
+
     // Column spacing between line profiles
     int _lineprofile_col_spacing;
+
     // Distance between values in line profile to derive
     int _lineprofile_derivative_distance;
+
     // Pair of vectors with crown points <upper crowns, lower crowns>
     pair< vector<cv::Point>, vector<cv::Point> > _crowns;
 
+    //// METHODS ////
     // Define upper and lower crown points
     void DefineCrownPoints();
+
     // Obtain derivatives of the vertical line profiles of image
     vector< pair< int, vector<int> > > DerivativeLineProfiles(const cv::Mat&, const int&, const int&);
 
     //// HELPFUL VISUALIZATION METHODS ////
     // Mark an X at input point
     cv::Mat DrawXAtPoints(const cv::Mat&, const vector<cv::Point>&, const cv::Vec3b&, const int& = 2);
+
     // Show display image
     void ShowDisplayImage();
 
