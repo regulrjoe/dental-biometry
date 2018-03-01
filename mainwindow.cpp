25a537c1320317539265f5e612fef3f447797b17
regulrjoe <joevzq@gmail.com>
2018-02-28 23:36:39 -0600

    Add controls and slots to UI for first testing

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,58 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_sliderMedian_sliderMoved(int position)
+{
+
+}
+
+void MainWindow::on_sliderBilateral_sliderMoved(int position)
+{
+
+}
+
+void MainWindow::on_numMedian_valueChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_numBilateral_valueChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_btnApplyMedian_clicked()
+{
+
+}
+
+void MainWindow::on_btnApplyBilateral_clicked()
+{
+
+}
+
+void MainWindow::on_btnClearFilters_clicked()
+{
+
+}
+
+void MainWindow::on_spinBox_3_valueChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_spinBox_4_valueChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
+
+void MainWindow::on_actionOpen_Image_triggered()
+{
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,29 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_sliderMedian_sliderMoved(int position);
+
+    void on_sliderBilateral_sliderMoved(int position);
+
+    void on_numMedian_valueChanged(int arg1);
+
+    void on_numBilateral_valueChanged(int arg1);
+
+    void on_btnApplyMedian_clicked();
+
+    void on_btnApplyBilateral_clicked();
+
+    void on_btnClearFilters_clicked();
+
+    void on_spinBox_3_valueChanged(int arg1);
+
+    void on_spinBox_4_valueChanged(int arg1);
+
+    void on_pushButton_clicked();
+
+    void on_actionOpen_Image_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
